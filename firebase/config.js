ec54002076fcd98c875120237cff9743a7fd4c9c
Sidra <szd123098@gmail.com>
2023-08-20 01:08:01 -0700

    changes/done

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAM4XIXK813utCVbTCcoOCkLDiaRTkoM0Q",
+    authDomain: "fir-project-a00f8.firebaseapp.com",
+    databaseURL: "https://fir-project-a00f8-default-rtdb.firebaseio.com",
+    projectId: "fir-project-a00f8",
+    storageBucket: "fir-project-a00f8.appspot.com",
+    messagingSenderId: "33408180361",
+    appId: "1:33408180361:web:d45feabd5f78c44d7ae658",
+    measurementId: "G-DRV9B90MZW"
+};
